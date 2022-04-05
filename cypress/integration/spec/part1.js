d082ed4deb336813b5deb1480d46851db4b0e616
Adam Szychliński <adam.szychlinski@itmagination.com>
2022-04-05 19:40:17 +0200

    do 4c, cosmetic whitespace change

--- cypress/integration/spec/part1.js
+++ cypress/integration/spec/part1.js
@@ -23,9 +23,26 @@ describe('Part 1', () => {
         // Wprowadzamy cześć danych i próbujemy wysłać - sprawdzamy komunikat błędu
 
         cy.visit('/Contact-Us/contactus.html')
+
         cy.get('[placeholder="Email Address"]').type('adam.szychlinski@itmagination.com')
+
         cy.get('[type="submit"][value="SUBMIT"]').click()
-        cy.get('body').contains('Error: all fields are requiredd')
+        cy.get('body').contains('Error: all fields are required')
+
+    })
+
+    it('4c', () => {
+        // Wprowadzamy błędny email i sprawdzamy komunikat
+
+        cy.visit('/Contact-Us/contactus.html')
+
+        cy.get('[placeholder="First Name"]').type('Adam')
+        cy.get('[placeholder="Last Name"]').type('Szychliński')
+        cy.get('[placeholder="Email Address"]').type('adam.szychlinski')
+        cy.get('[placeholder="Comments"]').type('lubię placki')
+
+        cy.get('[type="submit"][value="SUBMIT"]').click()
+        cy.get('body').contains('Error: Invalid email address')
 
     })
 
