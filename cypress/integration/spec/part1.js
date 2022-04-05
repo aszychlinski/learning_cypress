6b123298107514a7145c53c81c92dd257ecb4af2
Adam Szychliński <adam.szychlinski@itmagination.com>
2022-04-05 19:25:51 +0200

    create suite, do 4a

---
+++ cypress/integration/spec/part1.js
@@ -0,0 +1,22 @@
+describe('Part 1', () => {
+
+    it('4a', () => {
+        // Uzupełniamy wszystkie dane, i resetujemy - weryfikujemy czy wyczyściło poprawnie
+
+        cy.visit('/Contact-Us/contactus.html')
+
+        cy.get('[placeholder="First Name"]').type('Adam')
+        cy.get('[placeholder="Last Name"]').type('Szychliński')
+        cy.get('[placeholder="Email Address"]').type('adam.szychlinski@itmagination.com')
+        cy.get('[placeholder="Comments"]').type('lubię placki')
+
+        cy.get('[type="reset"][value="RESET"]').click()
+
+        cy.get('[placeholder="First Name"]').should('have.value', '')
+        cy.get('[placeholder="Last Name"]').should('have.value', '')
+        cy.get('[placeholder="Email Address"]').should('have.value', '')
+        cy.get('[placeholder="Comments"]').should('have.value', '')
+
+    })
+
+})
