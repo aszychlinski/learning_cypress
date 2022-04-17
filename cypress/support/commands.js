90c6bbf2fcd519cba51cd713dc1a2329a33239fb
Adam Szychliński <adam.szychlinski@itmagination.com>
2022-04-17 17:58:53 +0200

    fix formatting in commands.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -26,8 +26,8 @@
 
 Cypress.Commands.add('generateString', (length) => {
     // to nie działa - do zapytania
-    let result           = '';
-    const characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     const charactersLength = characters.length;
     for ( let i = 0; i < length; i++ ) {
         result += characters.charAt(Math.floor(Math.random() * charactersLength));
